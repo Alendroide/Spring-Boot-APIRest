868c3f4dad34344ff100e73ba88e0c6e5b13225b
Alendroide <alendroideyt@gmail.com>
2024-09-08 18:14:31 -0500

    CORS allowed

--- src/main/java/com/ejemplo/relacion/controller/EspecieController.java
+++ src/main/java/com/ejemplo/relacion/controller/EspecieController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ejemplo.relacion.entity.Especie;
 import com.ejemplo.relacion.service.EspecieService;
-
+@CrossOrigin(origins = "http://192.168.0.103:3000")
 @RestController
 @RequestMapping("/especies")
 public class EspecieController {
